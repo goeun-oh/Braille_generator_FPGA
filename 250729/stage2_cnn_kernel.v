4fe95a583fd3263fe5df25d813c80e1075aac7a3
DongEunKim123 <0905aude@gmail.com>
2025-07-29 16:29:27 +0900

    kernel pipelining 완료

--- 250729/stage2_cnn_kernel.v
+++ 250729/stage2_cnn_kernel.v
@@ -15,7 +15,7 @@ output                                         o_ot_valid  	,
 output    signed [`AK_BW-1 : 0]  			   o_ot_kernel_acc           
     );
 
-localparam LATENCY = 3+50;
+localparam LATENCY = 3+25;
 
 integer i,j,c;
 //==============================================================================
@@ -143,60 +143,83 @@ always @(posedge clk or negedge reset_n) begin
         	acc_kernel[c] <= 0;
 		end
 		r_acc_kernel<=0;
-    end else if (r_valid[1])  begin
+    end
+	if (r_valid[1])  begin
 		acc_kernel[0] <= 	r_mul[0][0][0];
-	end else if (r_valid[3]) begin
+	end 
+	if (r_valid[2]) begin
 		acc_kernel[1] <= acc_kernel[0] + r_mul[1][0][1];
-	end else if (r_valid[5]) begin
+	end
+	if (r_valid[3]) begin
 		acc_kernel[2] <= acc_kernel[1] + r_mul[2][0][2];
-	end else if (r_valid[7]) begin
+	end 
+	if (r_valid[4]) begin
 		acc_kernel[3] <= acc_kernel[2] + r_mul[3][0][3];
-	end else if (r_valid[9]) begin
+	end 
+	if (r_valid[5]) begin
 		acc_kernel[4] <= acc_kernel[3] + r_mul[4][0][4];
-	end else if (r_valid[11]) begin
+	end 
+	if (r_valid[6]) begin
 		acc_kernel[5] <= acc_kernel[4] + r_mul[5][1][0];
-	end else if (r_valid[13]) begin
+	end 
+	if (r_valid[7]) begin
 		acc_kernel[6] <= acc_kernel[5] + r_mul[6][1][1];
-	end else if (r_valid[15]) begin
+	end 
+	if (r_valid[8]) begin
 		acc_kernel[7] <= acc_kernel[6] + r_mul[7][1][2];
-	end else if (r_valid[17]) begin
+	end 
+	if (r_valid[9]) begin
 		acc_kernel[8] <= acc_kernel[7] + r_mul[8][1][3];
-	end else if (r_valid[19]) begin
+	end 
+	if (r_valid[10]) begin
 		acc_kernel[9] <= acc_kernel[8] + r_mul[9][1][4];
-	end else if (r_valid[21]) begin
+	end 
+	if (r_valid[11]) begin
 		acc_kernel[10] <= acc_kernel[9] + r_mul[10][2][0];
-	end else if (r_valid[23]) begin
+	end 
+	if (r_valid[12]) begin
 		acc_kernel[11] <= acc_kernel[10] + r_mul[11][2][1];
-	end else if (r_valid[25]) begin
+	end 
+	if (r_valid[13]) begin
 		acc_kernel[12] <= acc_kernel[11] + r_mul[12][2][2];
-	end else if (r_valid[27]) begin
+	end 
+	if (r_valid[14]) begin
 		acc_kernel[13] <= acc_kernel[12] + r_mul[13][2][3];
-	end else if (r_valid[29]) begin
+	end 
+	if (r_valid[15]) begin
 		acc_kernel[14] <= acc_kernel[13] + r_mul[14][2][4];
-	end else if (r_valid[31]) begin
+	end 
+	if (r_valid[16]) begin
 		acc_kernel[15] <= acc_kernel[14] + r_mul[15][3][0];
-	end else if (r_valid[33]) begin
+	end 
+	if (r_valid[17]) begin
 		acc_kernel[16] <= acc_kernel[15] + r_mul[16][3][1];
-	end else if (r_valid[35]) begin
+	end 
+	if (r_valid[18]) begin
 		acc_kernel[17] <= acc_kernel[16] + r_mul[17][3][2];
-	end else if (r_valid[37]) begin
+	end 
+	if (r_valid[19]) begin
 		acc_kernel[18] <= acc_kernel[17] + r_mul[18][3][3];
-	end else if (r_valid[39]) begin
-		acc_kernel[29] <= acc_kernel[18] + r_mul[19][3][4];	
-	end else if (r_valid[41]) begin
+	end 
+	if (r_valid[20]) begin
+		acc_kernel[19] <= acc_kernel[18] + r_mul[19][3][4];	
+	end 
+	if (r_valid[21]) begin
 		acc_kernel[20] <= acc_kernel[19] + r_mul[20][4][0];	
-	end else if (r_valid[43]) begin
+	end 
+	if (r_valid[22]) begin
 		acc_kernel[21] <= acc_kernel[20] + r_mul[21][4][1];	
-	end else if (r_valid[45]) begin
+	end 
+	if (r_valid[23]) begin
 		acc_kernel[22] <= acc_kernel[21] + r_mul[22][4][2];	
-	end else if (r_valid[47]) begin
+	end 
+	if (r_valid[24]) begin
 		acc_kernel[23] <= acc_kernel[22] + r_mul[23][4][3];		
-	end else if (r_valid[49]) begin
+	end 
+	if (r_valid[25]) begin
 		acc_kernel[24] <= acc_kernel[23] + r_mul[24][4][4];										
-	end else if (r_valid[51]) begin
-		for(c=0; c<`KX*`KY; c=c+1 ) begin
-        	acc_kernel[c] <= 0;
-		end
+	end 
+	if (r_valid[26]) begin
 		r_acc_kernel <= acc_kernel[24];
 	end
 end
@@ -257,7 +280,7 @@ end
 //                     r_partial_sum[2] + r_partial_sum[3] + r_partial_sum[4];
 // end
 
-assign o_ot_valid = r_valid[52];
+assign o_ot_valid = r_valid[27];
 assign o_ot_kernel_acc = r_acc_kernel;
 
 endmodule
